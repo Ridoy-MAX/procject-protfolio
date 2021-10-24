571341dec37aa704ee438316bec193078a8d14fd
Ridoy-MAX <pou4w98y@gmail.com>
2021-10-25 02:47:41 +0600

    smooth scroll

--- js/custom.js
+++ js/custom.js
@@ -26,7 +26,23 @@ this.classList.add('active');
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  reset: true
+})
 
+sr.reveal(`.easy-writing, .img`,{delay: 400})
+sr.reveal(`.quality`,{delay: 300})
+sr.reveal(`.banner-slider`,{delay: 300, origin: 'top'})
+
+sr.reveal(`.work, .products__card, .footer__logo, .footer__content, .footer__copy`,{origin: 'top', interval: 50})
+sr.reveal(`.our-feature, .discount__animate`,{origin: 'left', interval: 50})
+sr.reveal(`.footer-details, .menu-item`,{origin: 'right'})
+sr.reveal(`.our-service`,{origin: 'top'})
+sr.reveal(`.bank-card`)
 
 
 
